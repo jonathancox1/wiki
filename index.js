9c3725c1da5849ed2f1c16d49d23764b514aa56f
Sarah and Jon <cox.jonathan@gmail.com>
2022-02-19 15:34:07 -0500

    config

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
-const fs = require('fs').promises;
-const marked = require('marked');
+const fs = require("fs").promises;
+const marked = require("marked");
 
-const siteName = 'Rysolv';
-const baseUrl = 'https://rysolv.com/blog';
-const [today] = new Date().toISOString().split('T');
+const siteName = "wiki.jonathan-cox.dev";
+const baseUrl = "https://wiki.jonathan-cox.dev";
+const [today] = new Date().toISOString().split("T");
 const sitemap = [];
 
 async function build() {
-	const files = await fs.readdir('./src');
-	for (const file of files) {
-		if (file !== 'assets') {
-			// Take first part of file name for html name (this will be the url)
-			const [path] = file.split('.');
+  const files = await fs.readdir("./src");
+  for (const file of files) {
+    if (file !== "assets" && file !== "index") {
+      // Take first part of file name for html name (this will be the url)
+      const [path] = file.split(".");
 
-			// Read Markdown and generate HTML
-			const data = await fs.readFile(`./src/${file}`, 'utf8');
-			const html = generateHtml(data, path);
+      // Read Markdown and generate HTML
+      const data = await fs.readFile(`./src/${file}`, "utf8");
+      const html = generateHtml(data, path);
 
-			// Write HTML to file
-			await fs.writeFile(`./build/${path}.html`, html);
-		}
-	}
-	// Write sitemap.xml to file
-	await fs.writeFile(
-		`./build/sitemap.xml`,
-		[
-			'<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84 https://www.google.com/schemas/sitemap/0.84/sitemap.xsd">',
-			...sitemap,
-			'</urlset>',
-		].join('')
-	);
+      // Write HTML to file
+      await fs.writeFile(`./build/${path}.html`, html);
+    }
+  }
+  // Write sitemap.xml to file
+  await fs.writeFile(
+    `./build/sitemap.xml`,
+    [
+      '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84 https://www.google.com/schemas/sitemap/0.84/sitemap.xsd">',
+      ...sitemap,
+      "</urlset>",
+    ].join("")
+  );
 }
 
 function generateHtml(data, path) {
-	const [meta, markdown] = data.split('@@@');
+  const [meta, markdown] = data.split("@@@");
 
-	// Convert Meta tags to object
-	const metaObj = meta.split(/\n/).reduce((acc, el) => {
-		if (el.length) {
-			const [key, value] = el.split('=');
-			acc[key] = value.trim();
-		}
-		return acc;
-	}, {});
-	metaObj.url = `${baseUrl}/${path}.html`;
+  // Convert Meta tags to object
+  const metaObj = meta.split(/\n/).reduce((acc, el) => {
+    if (el.length) {
+      const [key, value] = el.split("=");
+      acc[key] = value.trim();
+    }
+    return acc;
+  }, {});
+  metaObj.url = `${baseUrl}/${path}.html`;
 
-	// Generte meta tags and HTML
-	const metaTags = generateMetaTags(metaObj);
-	const parsed = marked.parse(markdown);
+  // Generte meta tags and HTML
+  const metaTags = generateMetaTags(metaObj);
+  const parsed = marked.parse(markdown);
 
-	// Push articles to sitemap
-	sitemap.push(
-		`<url><loc>${metaObj.url || ''}</loc><lastmod>${
-			metaObj.date || today
-		}</lastmod></url>`
-	);
+  // Push articles to sitemap
+  sitemap.push(
+    `<url><loc>${metaObj.url || ""}</loc><lastmod>${
+      metaObj.date || today
+    }</lastmod></url>`
+  );
 
-	return `
+  return `
 		<!DOCTYPE html>
 		<html lang="en">
 		${metaTags}
@@ -69,32 +69,32 @@ function generateHtml(data, path) {
 }
 
 function generateMetaTags({ description, image, title, url }) {
-	// Create meta tags for social links
-	const baseMeta = `
+  // Create meta tags for social links
+  const baseMeta = `
 		<!-- Base meta tags -->
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="description" content="${description || ''}" />
+		<meta name="description" content="${description || ""}" />
 		<meta name="language" content="english" />
-		<meta name="title" content="${title || ''}" />
+		<meta name="title" content="${title || ""}" />
 		<meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, maximum-scale=1.0, user-scalable=2">	
 	`;
-	const openGraph = `
+  const openGraph = `
 		<!-- OpenGraph -->
-		<meta property="og:description" content="${description || ''}" />
+		<meta property="og:description" content="${description || ""}" />
 		<meta property="og:image" content="${image}" />
-		<meta property="og:site_name" content="${siteName || ''}" />
-		<meta property="og:title" content=${title || ''} />
+		<meta property="og:site_name" content="${siteName || ""}" />
+		<meta property="og:title" content=${title || ""} />
 		<meta property="og:type" content="article" />
-		<meta property="og:url" content="${url || ''}" />
+		<meta property="og:url" content="${url || ""}" />
 	`;
-	const twitter = `
+  const twitter = `
 		<!-- Twitter -->
 		<meta name="twitter:card" content="summary_large_image">
-		<meta name="twitter:description" content="${description || ''}" />
-		<meta name="twitter:image" content="${image || ''}" />
-		<meta name="twitter:title" content="${title || ''}" />
+		<meta name="twitter:description" content="${description || ""}" />
+		<meta name="twitter:image" content="${image || ""}" />
+		<meta name="twitter:title" content="${title || ""}" />
 	`;
-	return baseMeta + openGraph + twitter;
+  return baseMeta + openGraph + twitter;
 }
 build();
